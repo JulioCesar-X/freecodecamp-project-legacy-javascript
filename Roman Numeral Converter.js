ad4b54e1020d90d4812b94fb72bf28beedf31b7b
Júlio César Pereira <jc.imp10@gmail.com>
2024-03-06 10:51:48 +0000

    feat: add second project, a simple and strong function to solve the conversion from arabic numbers to roman numbers

---
+++ Roman Numeral Converter.js
@@ -0,0 +1,76 @@
+/*
+Convert the given number into a roman numeral.
+
+Roman numerals	Arabic numerals
+M	1000
+CM	900
+D	500
+CD	400
+C	100
+XC	90
+L	50
+XL	40
+X	10
+IX	9
+V	5
+IV	4
+I	1
+All roman numerals answers should be provided in upper-case.
+
+*/
+
+function convertToRoman(num) {
+    const romanNumerals = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    };
+    
+    let result = '';
+    
+    for (let key in romanNumerals)
+    {
+        while (num >= romanNumerals[key])
+        {
+            result += key;
+            num -= romanNumerals[key];
+        }
+    }
+    return result;
+}
+
+//Tests
+console.log(convertToRoman(4));
+console.log(convertToRoman(5));
+console.log(convertToRoman(9));
+console.log(convertToRoman(12));
+console.log(convertToRoman(16));
+console.log(convertToRoman(29));
+console.log(convertToRoman(44));
+console.log(convertToRoman(45));
+console.log(convertToRoman(68));
+console.log(convertToRoman(83));
+console.log(convertToRoman(97));
+console.log(convertToRoman(99));
+console.log(convertToRoman(400));
+console.log(convertToRoman(500));
+console.log(convertToRoman(501));
+console.log(convertToRoman(649));
+console.log(convertToRoman(798));
+console.log(convertToRoman(891));
+console.log(convertToRoman(1000));
+console.log(convertToRoman(1004));
+console.log(convertToRoman(1006));
+console.log(convertToRoman(1023));
+console.log(convertToRoman(2014));
+console.log(convertToRoman(3999));
